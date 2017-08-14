ee69d8db37f64204123bd925aed4e97982d433dd
Chris Limbrick <climbrick1989@gmail.com>
2017-08-14 07:24:56 -0400

    Adding new content

--- src/client/scripts/components/About.js
+++ src/client/scripts/components/About.js
@@ -8,7 +8,9 @@ export default class About extends React.Component {
   render() {
     return (
       <div className="about js-showtime">
-        <div className="about__title about__title--current-job">I built and launched impactful products at <a href="http://www.codeandcraft.nyc" target="_blank">Code and Craft</a> through user research, wireframing, and rapid development.
+        <div className="about__title about__title--current-job">Currently tackling the music industry at <a href="http://www.ascap.com" target="_blank">ASCAP</a> by improving the experiences of multiple digital touchpoints.
+        </div>
+        <div className="about__title about__title--previous-job">I built and launched impactful products at <a href="http://www.codeandcraft.nyc" target="_blank">Code and Craft</a> through user research, wireframing, and rapid development.
         </div>
         <div className="about__title about__title--previous-job">Previously, I was working at <a href="https://squarespace.com" target="_blank"> Squarespace</a> building the <a href="https://circle.squarespace.com" target="_blank">Circle</a> microsite and providing support for the <a href="https://developers.squarespace.com" target="_blank">Developer Platform</a>.</div>
         <div className="about__title about__title--hobbies">My hobbies include shooting film photography, DJing on turntables and occasionally jumping out of an airplane from 15,000 feet.</div>
